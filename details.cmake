b28db9156bd5ebaff80667424b7f07d088a57d92
Maik Beckmann <beckmann.maik@googlemail.com>
2011-04-29 21:40:30 +0200

    Docs: Added doxygen target `f2k3_lua_simple_docs' .

--- details.cmake
+++ details.cmake
@@ -19,3 +19,22 @@ set(DEFAULT_INSTALL_DESTINATIONS
   LIBRARY DESTINATION ${_library_dir} 
   ARCHIVE DESTINATION lib
 )
+
+
+# Maybe useful for C++ application and module writers, thus a reusable function
+function(generate_doxygen_documentation _target_name _doxygen_config_file)
+  find_package(Doxygen)
+  if(NOT DOXYGEN_FOUND )
+    message(STATUS "Doxygen not found - Doxygen based docs won't be generated")
+    return()
+  endif()
+
+  if(NOT EXISTS ${_doxygen_config_file})
+    message( STATUS "Doxygen configuration file not found - Doxygen based docs won't be generated")
+    return()
+  endif()
+
+  add_custom_target(${_target_name}
+    COMMAND ${DOXYGEN_EXECUTABLE} "${DOXYGEN_CONFIG_FILE}"
+  )
+endfunction(generate_doxygen_documentation)
